2216df124477d8a183853713add79e12f7a6a4f2
Rahman <museabdirahman@gmail.com>
2024-07-09 11:38:25 +0100

    initialised project , added dependencies and created homepage

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+interface ButtonProps {
+  isLoading: boolean;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      disabled={isLoading}
+      className={className ?? "shad-primary-btn w-full"}
+    >
+      {isLoading ? (
+        <div className="flex items-center gap-4">
+          <Image
+            src="/assets/icons/loader.svg"
+            alt="loader"
+            className="animate-spin"
+            width={24}
+            height={24}
+          />
+          Loading...
+        </div>
+      ) : (
+        children
+      )}
+    </Button>
+  );
+};
+
+export default SubmitButton;
---
+++ lib/validation.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const UserFormValidation = z.object({
+  name: z
+    .string()
+    .min(2, "name must be at least 2 characters.")
+    .max(2, "name must be max 50 characters."),
+  email: z.string().email("Invalid email address format"),
+  phone: z
+    .string()
+    .refine(
+      (phone) => /^\+?[1-9]\d{1,14}$/.test(phone),
+      "Invalid phone number"
+    ),
+});
